5803554f4e823a5146fe714ad50681ae727cfc96
Lucas2646 <73755354+Lucas3646@users.noreply.github.com>
2026-09-02 09:46:58 +0200

    Add iOS SwiftUI app entry point

---
+++ ios/MarketWidgets/MarketWidgetsApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct MarketWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
